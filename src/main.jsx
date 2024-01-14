19f87f6c14874474c2f563a1b52cd9635b26937b
jewelCode <jewelbarua101@gmail.com>
2024-01-14 19:37:54 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Root from './routes/root'
+import ReactDOM from 'react-dom/client'
+import ErrorPage from "./routes/ErrorPage";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+     <RouterProvider router={router} />
+  </React.StrictMode>,
+)
