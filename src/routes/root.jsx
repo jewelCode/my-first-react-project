c5424ad86cc5f8d9a7e6e94f1ba4583ebac0ef98
jewelCode <jewelbarua101@gmail.com>
2024-01-14 19:46:36 +0600

    new text added

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -3,6 +3,7 @@ const root = () => {
     return (
         <div>
             <h1>Hello World</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil, et. Laudantium accusantium veniam, eligendi odio non porro laboriosam eum perferendis maxime, recusandae quo saepe voluptatibus sequi exercitationem officia, ducimus commodi.</p>
         </div>
     );
 };
